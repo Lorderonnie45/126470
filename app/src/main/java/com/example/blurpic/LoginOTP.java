bd3fcc303c8def2053ab9b6ca49eb31f614fd71a
Richard Ronald <55141654+lorderonnie@users.noreply.github.com>
2025-04-11 08:38:55 +0300

    updated Main Menu

--- app/src/main/java/com/example/blurpic/LoginOTP.java
+++ app/src/main/java/com/example/blurpic/LoginOTP.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,7 @@ public class LoginOTP extends AppCompatActivity {
     TextView resendOtpTextView;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,17 +57,26 @@ public class LoginOTP extends AppCompatActivity {
 
         sendOtp(phoneNumber,false);
 
+        String phone = getSharedPreferences("userPrefs", MODE_PRIVATE).getString("userPhone", "+254711188899");
+
+        SharedPreferences.Editor editor = getSharedPreferences("userPrefs", MODE_PRIVATE).edit();
+        editor.putString("phone", phone);
+        editor.apply();
+
+
         nextBtn.setOnClickListener(v -> {
-            String enteredOtp  = otpInput.getText().toString();
-            PhoneAuthCredential credential =  PhoneAuthProvider.getCredential(verificationCode,enteredOtp);
+            String enteredOtp = otpInput.getText().toString();
+            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, enteredOtp);
             signIn(credential);
         });
+
 
         resendOtpTextView.setOnClickListener((v)->{
             sendOtp(phoneNumber,true);
         });
 
     }
+
 
     void sendOtp(String phoneNumber,boolean isResend){
 //        startResendTimer();
